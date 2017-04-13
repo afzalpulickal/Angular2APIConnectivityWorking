edb8fe44a25df134fa27713e0742594f07cb4834
afzal <afzal@SYSLP247>
2017-04-13 17:24:23 +0530

    original

--- Angular2TypingApp/app/app.module.js
+++ Angular2TypingApp/app/app.module.js
@@ -6,9 +6,6 @@ System.register(["@angular/core", "@angular/platform-browser", "@angular/http", 
         else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
         return c > 3 && r && Object.defineProperty(target, key, r), r;
     };
-    var __metadata = (this && this.__metadata) || function (k, v) {
-        if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-    };
     var __moduleName = context_1 && context_1.id;
     var core_1, platform_browser_1, http_1, app_component_1, DALServiceBase_1, AppModule;
     return {
@@ -41,8 +38,7 @@ System.register(["@angular/core", "@angular/platform-browser", "@angular/http", 
                     declarations: [app_component_1.AppComponent],
                     bootstrap: [app_component_1.AppComponent],
                     providers: [DALServiceBase_1.DALServiceBase]
-                }),
-                __metadata("design:paramtypes", [])
+                })
             ], AppModule);
             exports_1("AppModule", AppModule);
         }
